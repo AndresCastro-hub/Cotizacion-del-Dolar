7957dfbddc8b50017bc45b7757caf667fef90688
Andres Castro <andrescastro2011@hotmail.com>
2022-05-11 19:36:38 -0300

    pintar las cards con la api

--- src/components/Cotizador.js
+++ src/components/Cotizador.js
@@ -1,72 +1,93 @@
 import React, { useEffect, useState } from 'react'
+import './style.css'
+import  logo from'../assets/logo.png'
+import  logoDolar from'../assets/simbolo.png'
+import  euro from'../assets/euro.png'
 
 export const Cotizador = () => {
-
+    
     const [datos, SetDatos] = useState([])
-
+    
     const fetchApi = async () => {
-
+        
         const url = 'https://api.bluelytics.com.ar/v2/latest'
         const respuesta = await fetch(url)
 
         const data = await respuesta.json();
-                
-        SetDatos(data)
+        
+        SetDatos([data])
     }
 
     console.log(datos)
-
+    
     useEffect(( ) => {
         fetchApi();
     },[])
-
-    // const url = 'https://api.bluelytics.com.ar/v2/latest'
-
-    // const llamadoApi = () => {
-    //     fetch(url)
-    //     .then((respuesta) => respuesta.json())
-    //     .then((data) => {
-    //         SetDatos(data)
-    //     })
-    // }
-
-    // useEffect ( () => {
-    //     llamadoApi()
-    // },[])
-
-   
+  
     return (
         <>
 
-            
-            <h1>COTIZACION DEL DOLAR HOY, PRECIO DEL DOLAR HOY</h1>
+        <nav className='menu'>
+            <img className='menuLogo' src={logo}></img>
+            <p className='menuTexto'>DÓLAR</p>
+        </nav>
 
-            <h2>Lo que hay que saber del dólar hoy en la Argentina, con información completa y actualizada sobre la cotización del dólar en el Banco Nación, en el mercado mayorista y los datos del Banco Central. Tablas del dólar histórico, para poder analizar la evolución del tipo de cambio. Y todo el análisis del contexto para entender el mercado cambiario.</h2>
+      
 
-            <div className='dolarOficial'>
-                <h1>Dolar Oficial</h1>
-                <p>Compra:</p>
-                <p>Venta:</p>
+            <h1 className='containerTitle'>COTIZACION DEL DOLAR HOY, PRECIO DEL DOLAR HOY</h1>
+
+            <hr/>
+
+        {
+            datos.map ( data => {
+
+        return(
+
+            <div key={Math.random()} className="orden">
+
+                <div className='cardStyle card'>
+                    <h1>Dolar Oficial</h1>
+                    <p className='izquierda'>Compra: ${data.oficial.value_buy} <span className='derecha'>Venta: ${data.oficial.value_sell}</span></p>
+                    <img className='logoDolar' src={logoDolar}></img>
+                 
+                </div>
+
+                
+                <div className='cardStyle card'>
+                    <h1>Euro Oficial</h1>
+                    <p className='izquierda'>Compra:${data.oficial_euro.value_buy}  <span className='derecha'>Venta:${data.oficial_euro.value_sell}</span></p>
+                    <img className='logoDolar' src={euro}></img>
+                </div>
+    
+   
+                <div className='cardStyle card'>
+                    <h1>Dolar Blue</h1>
+                    <p className='izquierda'>Compra: ${data.blue.value_buy} <span className='derecha'>Venta:${data.blue.value_sell}</span></p>
+                    <img className='logoDolar' src={logoDolar}></img>
+                </div>
+
+  
+
+
+                <div className='cardStyle card'>
+                    <h1>Euro Blue</h1>
+                    <p className='izquierda'>Compra:${data.blue_euro.value_buy}  <span className='derecha'>Venta:${data.blue_euro.value_sell} </span></p>
+                    <img className='logoDolar' src={euro}></img>
+                </div>
+
+                
             </div>
+           
+            )})
+        }
 
-            <div className='dolarBlue'>
-                <h1>Dolar Blue</h1>
-                <p>Compra:</p>
-                <p>Venta:</p>
-            </div>
+        <p className='descripcion'>
+            Lo que hay que saber del dólar hoy en la Argentina, con información completa y actualizada sobre la cotización del dólar en el Banco Nación, en el mercado mayorista y los datos del Banco Central. Tablas del dólar histórico, para poder analizar la evolución del tipo de cambio. Y todo el análisis del contexto para entender el mercado cambiario.
+        </p>
 
-            <div className='euroOficial'>
-                <h1>Euro Oficial</h1>
-                <p>Compra:</p>
-                <p>Venta:</p>
-            </div>
-
-            <div className='euroBlue'>
-                <h1>Euro Blue</h1>
-                <p>Compra:</p>
-                <p>Venta:</p>
-            </div>
-
+        <footer className='footer'>
+            <p className='footerTexto'>Desarollado por Andres Castro*</p>
+        </footer>
 
         </>
     )
